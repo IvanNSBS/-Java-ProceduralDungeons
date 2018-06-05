9aaecd584432ebb517b8b41588179eb31ec52a24
IvanNSBS <insbs@cin.ufpe.br>
2018-06-05 11:47:16 -0300

    Update

--- ProceduralDungeons/src/Main.java
+++ ProceduralDungeons/src/Main.java
@@ -1,23 +1,30 @@
+import java.security.SecureRandom;
+
 public class Main
 {
-	private static int Width = 40;
-	private static int Height = 20;
-	private static float FillChance = 0.5f;
-	private static int Iterations = 3;
+	private static int Width = 96;
+	private static int Height = 40;
+	private static float FillChance = 0.42f;
+	private static int Iterations = 2;
+	
+	private static SecureRandom srandom = new SecureRandom();
 	
 	private static boolean[][] Chunk = new boolean[Width][Height]; 
 	
-	private static String Fill = "#"; 
-	private static String Empty = " ";
+	private static String Fill = "██"; 
+	private static String Empty = "  ";
 	
-	private static int BirthLimit = 4;
-	private static int DeathLimit = 3;
+	private static int M1BirthLimit = 4;
+	private static int M1DeathLimit = 3;
+	
+	private static int M2BirthLimit = 4;
+	private static int M2DeathLimit = 5;
 	
 	public static void RandomizeChunk() 
 	{
 		for(int y = 0; y < Height; y++)
 			for(int x = 0; x < Width; x++) 
-				Chunk[x][y] = (Math.random() <= FillChance) ? true : false;
+				Chunk[x][y] = (srandom.nextFloat() < FillChance) ? true : false;
 	}
 	
 	public static void PrintChunk() 
@@ -31,8 +38,10 @@ public class Main
 				else
 					System.out.print(Empty);
 			}
-			System.out.println("");
+			if(y != Height-1)
+				System.out.println("");
 		}
+		System.out.println("\nEnded");
 	}
 	
 	public static int GetNeighbors(int x, int y) 
@@ -50,7 +59,7 @@ public class Main
 		return count;
 	} 
 	
-	public static void GenerateChunk() 
+	public static void GenerateChunkM1() 
 	{
 		for(int i = 0; i < Iterations; i++) 
 		{
@@ -61,7 +70,7 @@ public class Main
 				{
 					if(Chunk[x][y]) 
 					{						
-						if(GetNeighbors(x,y) < DeathLimit)
+						if(GetNeighbors(x,y) < M1DeathLimit)
 							temp[x][y] = false;
 						else
 							temp[x][y] = true;
@@ -69,7 +78,7 @@ public class Main
 					
 					else 
 					{
-						if(GetNeighbors(x,y) > BirthLimit)
+						if(GetNeighbors(x,y) > M1BirthLimit)
 							temp[x][y] = true;
 						else
 							temp[x][y] = false;
@@ -79,15 +88,70 @@ public class Main
 			Chunk = temp;
 		}
 	}
+
+	public static void GenerateChunkM2() 
+	{
+		for(int i = 0; i < Iterations; i++) 
+		{
+			boolean[][] temp = Chunk;
+			for(int y = 0; y < Height; y++) 
+			{
+				for(int x = 0; x < Width; x++) 
+				{
+					if(Chunk[x][y]) 
+					{						
+						if(GetNeighbors(x,y) >= M2BirthLimit)
+							temp[x][y] = true;
+						if(GetNeighbors(x,y) <= 2)
+							temp[x][y] = false;
+					}
+					
+					else 
+					{
+						if(GetNeighbors(x,y) >= M2DeathLimit)
+							temp[x][y] = true;
+					}
+				}
+			}
+			Chunk = temp;
+		}
+	}
+	
+	public static void FinishChunk() 
+	{
+		for(int i = 0; i < 3; i++) 
+		{
+			for(int y = 0; y < Height; y++) 
+			{
+				for(int x = 0; x < Width; x++) 
+				{
+					int NC = GetNeighbors(x,y);
+					if(Chunk[x][y]) 
+					{
+						if(NC <= 2)
+							Chunk[x][y] = false;
+					}
+					else
+						if(NC >= 4)
+							Chunk[x][y] = false;
+				}
+			}
+		}
+	}
 	
 	public static void main(String args[]) 
 	{
 		RandomizeChunk();
+		GenerateChunkM1();
+		FinishChunk();
 		PrintChunk();
 		
-		System.out.println("");
+		System.out.println("\n\n");
 		
-		GenerateChunk();
+		RandomizeChunk();
+		GenerateChunkM2();
+		FinishChunk();
 		PrintChunk();
+		
 	} 
 }
